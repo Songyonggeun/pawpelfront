4869befae9b9f23b97a820ded648d94ff784dbb6
Songyonggeun <yg38ant@gmail.com>
2025-05-16 19:41:45 +0900

    삭제된거 다시 올림
    서브 카테고리 수정

---
+++ components/(application)/communityMenu.jsx
@@ -0,0 +1,17 @@
+export default function CommunityMenu() {
+  return (
+    <div className="flex flex-col md:flex-row md:items-center justify-between flex-wrap gap-4 text-sm text-gray-600 p-4 max-w-7xl mx-auto">
+      <ul className="flex flex-col md:flex-row gap-4">
+        <li><a href="/community/home" className="hover:underline block">홈</a></li>
+        <li><a href="/community/all" className="hover:underline block">전체글</a></li>
+        <li><a href="/community/topic" className="hover:underline block">건강토픽</a></li>
+        <li><a href="/community/best" className="hover:underline block">BEST</a></li>
+        <li><a href="/community/qa" className="hover:underline block">질문과답</a></li>
+        <li><a href="/community/daily" className="hover:underline block">건강일상</a></li>
+        <li><a href="/community/knowhow" className="hover:underline block">노하우</a></li>
+        <li><a href="/community/psychology" className="hover:underline block">심리케어</a></li>
+        <li><a href="/community/wiki" className="hover:underline block">라이펫 위키</a></li>
+      </ul>
+    </div>
+  );
+}
---
+++ components/(application)/healthCare.jsx
@@ -0,0 +1,12 @@
+export default function HealthCareMenu() {
+  return (
+    <div className="flex flex-col md:flex-row md:items-center justify-between flex-wrap gap-4 text-sm text-gray-600 p-4 max-w-7xl mx-auto ">
+      <ul className="flex flex-col md:flex-row gap-4">
+        <li><a href="/health/home" className="hover:underline block">건강홈</a></li>
+        <li><a href="/health/check" className="hover:underline block">건강체크</a></li>
+        <li><a href="/health/consult" className="hover:underline block">수의사상담</a></li>
+        <li><a href="/health/guide" className="hover:underline block">애완동물 건강체크 가이드</a></li>
+      </ul>
+    </div>
+  );
+}
