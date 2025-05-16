c84324444a7afbc0e6095e910a5818c1c3daf33e
raytear <younji.mok@gmail.com>
2025-05-16 17:55:47 +0900

    Update page.jsx
    테스트임

--- app/(application)/signup/page.jsx
+++ app/(application)/signup/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SignUp() {
   return (
     <div>
-      <h1>테스트 문장</h1>
+      <h1>로그인 테스트 문장</h1>
       <p>이 페이지는 /home 경로에 표시되는 테스트 페이지입니다.</p>
     </div>
   );
