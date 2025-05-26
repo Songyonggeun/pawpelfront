42ef36e689b81492d2364810d2bd82fec4c04b07
Hongrae219 <zxhl235655@gmail.com>
2025-05-26 19:01:51 +0900

    Update page.jsx

--- app/(application)/login/page.jsx
+++ app/(application)/login/page.jsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          className="w-full px-4 py-2 mb-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full px-2 py-2 mb-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
 
         <div className="flex justify-center gap-x-5 text-sm text-blue-600 mb-6">
