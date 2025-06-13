a056cee516310392c87165574946eaa8ebd1fcf4
Hongrae219 <zxhl235655@gmail.com>
2025-06-13 14:38:54 +0900

    Update footer.jsx

--- components/(application)/footer.jsx
+++ components/(application)/footer.jsx
@@ -1,4 +1,3 @@
-'use client';
 
 export default function Footer() {
   return (
@@ -7,21 +6,21 @@ export default function Footer() {
         {/* 왼쪽: 로고 및 회사 정보 */}
         <div className="flex flex-col space-y-2">
           <div className="flex items-center space-x-2">
-            <span className="text-blue-500 text-xs font-bold">✓</span>
-            <span className="text-sm font-bold text-blue-500">Pawple</span>
+            <img src="/logo.svg" alt="Lifet Logo" className="w-6 h-6" />
+            <span className="text-blue-500 font-bold text-lg">Lifet</span>
           </div>
           <div className="text-xs space-y-0.5">
-            <p>상호: 주식회사 포플 | 대표자: 홍길동 | 개인정보취급담당자: 홍길동</p>
-            <p>사업자등록번호: 111-22-33333</p>
-            <p>주소: 서울 노원구 상계로1길 34 세일학원 5층</p>
-            <p>통신판매업신고: 제2024-서울노원-01234호</p>
-            <p>© Pawple Inc. All Rights Reserved</p>
+            <p>상호: 주식회사 일레븐리터 | 대표자: 김광현 | 개인정보취급담당자: 김광현</p>
+            <p>사업자등록번호: 896-81-02510</p>
+            <p>주소: 서울특별시 강남구 역삼로160, 8층 801호</p>
+            <p>통신판매업신고: 제2024-서울강남-07247호</p>
+            <p>© Elevenliter Inc. All Rights Reserved</p>
           </div>
         </div>
 
         {/* 중간: 링크 */}
         <div className="space-y-1 text-xs">
-          <p className="font-semibold">포플 소개</p>
+          <p className="font-semibold">라이펫 소개</p>
           <div className="flex flex-wrap gap-x-4">
             <a href="#" className="hover:underline">개인정보처리방침</a>
             <a href="#" className="hover:underline">이용약관</a>
@@ -32,20 +31,20 @@ export default function Footer() {
         {/* 오른쪽: 고객센터 */}
         <div className="text-xs space-y-1">
           <p className="font-semibold text-gray-800">고객센터</p>
-          <p className="text-lg text-black font-bold">070-1111-2222</p>
+          <p className="text-lg text-black font-bold">070-4519-6419</p>
           <p>평일 11:00 ~ 18:00 | 점심시간 13:00 ~ 14:00<br />(주말/공휴일 휴무)</p>
-          <p>문의 메일: <a href="mailto:official@elevenliter.com" className="underline">official@pawple.com</a></p>
+          <p>문의 메일: <a href="mailto:official@elevenliter.com" className="underline">official@elevenliter.com</a></p>
         </div>
 
         {/* SNS 아이콘 */}
-        {/* <div className="flex space-x-2">
+        <div className="flex space-x-2">
           <a href="#" className="w-8 h-8 flex items-center justify-center border rounded-full">
             <img src="/icons/instagram.svg" alt="Instagram" className="w-4 h-4" />
           </a>
           <a href="#" className="w-8 h-8 flex items-center justify-center border rounded-full">
             <img src="/icons/kakao.svg" alt="Kakao" className="w-4 h-4" />
           </a>
-        </div> */}
+        </div>
       </div>
     </footer>
   );
